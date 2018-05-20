fdcc4e91ba1bf035af5b10ed95fb9b8bf33bff2b
Peggy Lee <peggyleesix@gmail.com>
2018-05-20 18:55:14 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
-board.each do |turn_count|
-  if #{turn_count} == "X" || "O"
+board.each do |board|
+  if board != " "
   counter += 1
   end
 end
